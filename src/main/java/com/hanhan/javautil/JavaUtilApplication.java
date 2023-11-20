1e1a8d88f37e914185ed322bdbc3adf4583d0333
彭谅 <pengliang@gwall.cn>
2023-11-20 09:29:34 +0800

    first commit

---
+++ src/main/java/com/hanhan/javautil/JavaUtilApplication.java
@@ -0,0 +1,13 @@
+package com.hanhan.javautil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUtilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanhan/javautil/JavaUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanhan.javautil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
