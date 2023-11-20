793b74ac0b230c7a4c907b274e7f15c366a39fa5
彭谅 <pengliang@gwall.cn>
2023-11-20 09:56:23 +0800

    first commit

--- src/test/java/com/hanhan/javautil/JavaUtilApplicationTests.java
+++ src/test/java/com/hanhan/javautil/JavaUtilApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaUtilApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
